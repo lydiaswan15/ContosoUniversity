40d7fe034d80ddb84b9b5ce5a35df01145285f2a
lydiaswan15 <lydiaswan15@gmail.com>
2021-11-26 18:35:51 -0700

    Getting the 5.0 up and running!!! Woot woot!

--- ContosoUniversity/Models/ErrorViewModel.cs
+++ ContosoUniversity/Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace ContosoUniversity.Models;
+using System;
 
-public class ErrorViewModel
+namespace ContosoUniversity.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
